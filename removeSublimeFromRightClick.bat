ae23e2c16c1ca015bef93f761dac2cdab7d8e574
luong hai <lxhair17@gmail.com>
2023-10-28 16:18:08 +0700

    1st

---
+++ removeSublimeFromRightClick.bat
@@ -0,0 +1,7 @@
+@echo off
+@reg delete "HKEY_CLASSES_ROOT\*\shell\Open with Sublime"
+@reg delete "HKEY_CLASSES_ROOT\*\shell\Open with Sublime"
+@reg delete "HKEY_CLASSES_ROOT\*\shell\Open with Sublime\command"
+@reg delete "HKEY_CLASSES_ROOT\Folder\shell\Open with Sublime"
+@reg delete "HKEY_CLASSES_ROOT\Folder\shell\Open with Sublime"
+@reg delete "HKEY_CLASSES_ROOT\Folder\shell\Open with Sublime\command"
